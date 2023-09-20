4545f734e5ac38dde9cbd5674400a7c6762af0f7
Steve Butler <butlersteve.c@gmail.com>
2023-09-19 19:08:45 -0500

    tag and category pages complete

--- components/ListComponent.js
+++ components/ListComponent.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import PropTypes, { arrayOf } from 'prop-types';
+import PropTypes from 'prop-types';
 
-const ListComponent = ({ arr }) => (
+const ListComponent = ({ item }) => (
   <>
-    <table className="w-75" style={{ fontSize: '16px' }}>
-      {arr.map((item) => <tr style={{ border: '2px solid black', textAlign: 'center' }}><td className="p-2 w-25">{item.label}</td></tr>)}
-    </table>
+
+    <tr style={{ border: '2px solid black', textAlign: 'center' }}><td className="p-2 w-25">{item}</td></tr>
+
   </>
 );
 
 export default ListComponent;
 
 ListComponent.propTypes = {
-  arr: PropTypes.shape(arrayOf(PropTypes.string)).isRequired,
+  item: PropTypes.string.isRequired,
 };
---
+++ components/NewLabelForm.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { createCategory } from '../api/categoryData';
+import { createTag } from '../api/tagsData';
+
+const initialState = {
+  lable: '',
+};
+
+function NewLableForm({ onUpdate, type }) {
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = { ...formData };
+
+    if (type === 'category') {
+      createCategory(payload).then(() => onUpdate());
+    } else if (type === 'tag') {
+      createTag(payload).then(() => onUpdate());
+    }
+    e.target.reset();
+  };
+
+  return (
+    <>
+      <div style={{ border: '1px solid black', borderRadius: '10px', padding: '20px' }}>
+        <h4>Create a new { type === 'tag' ? <span>Tag</span> : <span>Category</span>}</h4>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" name="label" required onChange={handleChange} />
+          </Form.Group>
+          <Button variant="secondary" className="btn-sm" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </>
+
+  );
+}
+
+NewLableForm.propTypes = {
+  type: PropTypes.string.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default NewLableForm;
